b4d65fd9558f373b5995a25f41ea8291468c69bc
Manoel Milanesi <manumila88@gmail.com>
2026-06-05 19:08:49 +0200

    fix: ResponsiveContainer 99% minWidth

--- frontend/src/components/PriceHistoryChart.jsx
+++ frontend/src/components/PriceHistoryChart.jsx
@@ -81,7 +81,7 @@ export default function PriceHistoryChart({ wineId, currentPrice = null, height 
 
   return (
     <div style={{ width: "100%", height }}>
-      <ResponsiveContainer width="100%" height="100%">
+      <ResponsiveContainer width="99%" minWidth={200} height={height}>
         <ComposedChart data={data} margin={{ top: 4, right: 4, left: 0, bottom: 0 }}>
           <defs>
             <linearGradient id="priceRangeGrad" x1="0" y1="0" x2="0" y2="1">
